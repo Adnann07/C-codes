38d36d0b340f388181b4a105efd36799341679b2
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-07-30 01:00:06 +0600

    Add files via upload

---
+++ 2468.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int r=1;r<=4;r++)
+    {
+        for(int c=1;c<=5;c++)
+        {
+            cout<<r*2;
+        }
+        cout<<"\n";
+    }
+}
---
+++ number_triangle_rev.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int r=1;r<=5;r++)
+    {
+        for(int c=r;c<=5;c++)
+        {
+            cout<<r;
+        }
+        cout<<"\n";
+    }
+}
